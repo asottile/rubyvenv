769caf093a8f21fceffbb9735170a01129de1e4b
Anthony Sottile <asottile@umich.edu>
2016-09-18 22:12:33 -0700

    rubyvenv creates prebuilt environments

--- rubyvenv.py
+++ rubyvenv.py
@@ -3,10 +3,16 @@ from __future__ import unicode_literals
 
 import argparse
 import collections
+import contextlib
+import functools
 import gzip
 import io
 import os.path
+import pipes
 import platform
+import shutil
+import tarfile
+import tempfile
 
 import distro
 import six
@@ -19,11 +25,32 @@ Platform = collections.namedtuple('Platform', ('name', 'version', 'arch'))
 Version = collections.namedtuple('Version', ('version', 'url'))
 
 
-def cache_dir():
+def get_cache_dir():
     return os.path.join(
         os.getenv('XDG_CACHE_HOME', os.path.expanduser('~/.cache')),
         'rubyvenv',
     )
+
+
+def ensure_cache_file(relpath, get_fileobj):
+    cache_dir = get_cache_dir()
+    path = os.path.join(cache_dir, relpath)
+    if os.path.exists(path):
+        return path
+    else:
+        mkdirp(os.path.dirname(path))
+        # Write to a temporary file and then rename for atomicity
+        tmpdir = os.path.join(cache_dir, 'tmp')
+        mkdirp(tmpdir)
+        try:
+            with tempfile.NamedTemporaryFile(dir=tmpdir, delete=False) as dst:
+                with get_fileobj() as src:
+                    shutil.copyfileobj(src, dst)
+        except BaseException:
+            os.remove(dst.name)
+            raise
+        os.rename(dst.name, path)
+        return path
 
 
 def mkdirp(path):
@@ -66,6 +93,16 @@ def _filename_to_version(filename):
     return filename[len('ruby-'):-1 * len('.tar.bz2')]
 
 
+def _version_to_filename(version):
+    return 'ruby-{}.tar.bz2'.format(version)
+
+
+def _download_url(platform_info, version):
+    return six.moves.urllib_parse.urljoin(
+        RVM.format(**platform_info._asdict()), _version_to_filename(version),
+    )
+
+
 def get_prebuilt_versions(platform_info):
     url = RVM.format(**platform_info._asdict())
     resp = _decode_response(six.moves.urllib.request.urlopen(url).read())
@@ -92,9 +129,46 @@ def list_versions():
         print('    - {}'.format(version.version))
 
 
+def pick_version(version):
+    platform_info = get_platform_info()
+    if version != 'latest':
+        return Version(version, _download_url(platform_info, version))
+    else:
+        return get_prebuilt_versions(platform_info)[-1]
+
+
+def urlopen_closable(*args):
+    return contextlib.closing(six.moves.urllib.request.urlopen(*args))
+
+
+def make_environment(dest, version):
+    platform_info = get_platform_info()
+    filename = _version_to_filename(version.version)
+    cache_file = '{name}/{version}/{arch}/{filename}'.format(
+        filename=filename, **platform_info._asdict()
+    )
+    get_fileobj = functools.partial(urlopen_closable, version.url)
+    tar_filename = ensure_cache_file(cache_file, get_fileobj)
+    dest = os.path.abspath(dest)
+    mkdirp(dest)
+    with tarfile.open(tar_filename) as tar_file:
+        members = tar_file.getmembers()
+        # Remove the first path segment so we extract directly into the
+        # destination directory
+        for member in members:
+            if os.sep in member.name:
+                _, member.name = member.name.split(os.sep, 1)
+            else:
+                member.name = ''
+        tar_file.extractall(dest, members)
+    with io.open(os.path.join(dest, 'bin', 'activate'), 'w') as activate:
+        activate.write(ACTIVATE.replace('DIRECTORY', pipes.quote(dest)))
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('dest', nargs='?', metavar='DEST_DIR')
+    parser.add_argument('--ruby', default='latest')
     parser.add_argument(
         '--list-versions', action='store_true',
         help='List versions available for your system',
@@ -104,8 +178,67 @@ def main(argv=None):
     if args.list_versions:
         return list_versions()
     else:
-        raise NotImplementedError
+        if not args.dest:
+            parser.error('DEST_DIR is required')
+        version = pick_version(args.ruby)
+        return make_environment(args.dest, version)
 
+
+# Roughly stolen from python virtualenv 15.0.1
+ACTIVATE = '''\
+# This file must be used with "source bin/activate" *from bash*
+# you cannot run it directly
+
+deactivate_rubyvenv () {
+    # reset old environment variables
+    # ! [ -z ${VAR+_} ] returns true if VAR is declared at all
+    if ! [ -z "${_OLD_RUBYVENV_PATH+_}" ] ; then
+        PATH="$_OLD_RUBYVENV_PATH"
+        export PATH
+        unset _OLD_RUBYVENV_PATH
+    fi
+
+    # This should detect bash and zsh, which have a hash command that must
+    # be called to get it to forget past commands.  Without forgetting
+    # past commands the $PATH changes we made may not be respected
+    if [ -n "${BASH-}" ] || [ -n "${ZSH_VERSION-}" ] ; then
+        hash -r 2>/dev/null
+    fi
+
+    if ! [ -z "${_OLD_RUBYVENV_PS1+_}" ] ; then
+        PS1="$_OLD_RUBYVENV_PS1"
+        export PS1
+        unset _OLD_RUBYVENV_PS1
+    fi
+
+    unset RUBYVENV
+    if [ ! "${1-}" = "nondestructive" ] ; then
+        # Self destruct!
+        unset -f deactivate_rubyvenv
+    fi
+}
+
+# unset irrelevant variables
+deactivate_rubyvenv nondestructive
+
+RUBYVENV=DIRECTORY
+export RUBYVENV
+
+_OLD_RUBYVENV_PATH="$PATH"
+PATH="$RUBYVENV/bin:$PATH"
+export PATH
+
+_OLD_RUBYVENV_PS1="$PS1"
+PS1="($(basename "$RUBYVENV")) $PS1"
+export PS1
+
+# This should detect bash and zsh, which have a hash command that must
+# be called to get it to forget past commands.  Without forgetting
+# past commands the $PATH changes we made may not be respected
+if [ -n "${BASH-}" ] || [ -n "${ZSH_VERSION-}" ] ; then
+    hash -r 2>/dev/null
+fi
+'''
 
 if __name__ == '__main__':
     exit(main())
--- tests/rubyvenv_test.py
+++ tests/rubyvenv_test.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import io
 import os
 import platform
+import subprocess
 
 import distro
 import mock
@@ -14,9 +15,78 @@ import rubyvenv
 from testing.resources import resource
 
 
-def test_cache_dir_xdg_variable():
+@pytest.yield_fixture
+def mocked_cache(tmpdir):
+    cache_dir = tmpdir.join('.cache')
+    with mock.patch.dict(os.environ, {'XDG_CACHE_HOME': cache_dir.strpath}):
+        yield cache_dir.join('rubyvenv')
+
+
+def test_get_cache_dir_xdg_variable():
     with mock.patch.dict(os.environ, {'XDG_CACHE_HOME': '/foo'}):
-        assert rubyvenv.cache_dir() == '/foo/rubyvenv'
+        assert rubyvenv.get_cache_dir() == '/foo/rubyvenv'
+
+
+def test_mocked_cache(mocked_cache):
+    assert rubyvenv.get_cache_dir() == mocked_cache.strpath
+
+
+def _fileobj_func(contents):
+    def func():
+        return io.BytesIO(contents)
+    return func
+
+
+class BoomError(RuntimeError):
+    pass
+
+
+class RaisesAfterSomeIO(object):
+    def __init__(self):
+        self.until_boom = 3
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+    def read(self, size):
+        if self.until_boom:
+            self.until_boom -= 1
+            return b'tick tock!\n'
+        else:
+            raise BoomError()
+
+
+def test_ensure_cache_file_file_exists(mocked_cache):
+    cache_file = mocked_cache.ensure_dir().join('test.txt')
+    cache_file.write('foo')
+    ret = rubyvenv.ensure_cache_file('test.txt', _fileobj_func(b'bar'))
+    assert ret == cache_file.strpath
+    assert cache_file.read() == 'foo'
+
+
+def test_ensure_cache_file_file_does_not_exist(mocked_cache):
+    cache_file = mocked_cache.join('test.txt')
+    ret = rubyvenv.ensure_cache_file('test.txt', _fileobj_func(b'bar'))
+    assert ret == cache_file.strpath
+    assert cache_file.read() == 'bar'
+
+
+def test_ensure_cache_file_makes_directories(mocked_cache):
+    cache_file = mocked_cache.join('dir/test.txt')
+    ret = rubyvenv.ensure_cache_file('dir/test.txt', _fileobj_func(b'bar'))
+    assert ret == cache_file.strpath
+    assert cache_file.read() == 'bar'
+
+
+def test_ensure_cache_exception_safety(mocked_cache):
+    cache_file = mocked_cache.join('test.txt')
+    with pytest.raises(BoomError):
+        rubyvenv.ensure_cache_file('test.txt', RaisesAfterSomeIO)
+    assert not cache_file.exists()
+    assert mocked_cache.join('tmp').listdir() == []
 
 
 def test_mkdirp(tmpdir):
@@ -69,6 +139,26 @@ def test_filename_to_version(filename, expected):
     assert rubyvenv._filename_to_version(filename) == expected
 
 
+def test_version_to_filename():
+    assert rubyvenv._version_to_filename('1.2.3') == 'ruby-1.2.3.tar.bz2'
+
+
+def test_version_to_filename_filename_to_version_roundtrip():
+    version = '1.2.3'
+    ret = version
+    ret = rubyvenv._version_to_filename(ret)
+    ret = rubyvenv._filename_to_version(ret)
+    assert ret == version
+
+
+@pytest.yield_fixture
+def xenial():
+    with mock.patch.object(platform, 'machine', return_value='x86_64'):
+        with mock.patch.object(distro, 'id', return_value='ubuntu'):
+            with mock.patch.object(distro, 'version', return_value='16.04'):
+                yield
+
+
 @pytest.yield_fixture
 def returns_xenial():
     contents = io.open(resource('ubuntu_16_04_x86_64.htm.gzip'), 'rb').read()
@@ -112,14 +202,6 @@ def test_get_prebuilt_versions():
     )
 
 
-@pytest.yield_fixture
-def xenial():
-    with mock.patch.object(platform, 'machine', return_value='x86_64'):
-        with mock.patch.object(distro, 'id', return_value='ubuntu'):
-            with mock.patch.object(distro, 'version', return_value='16.04'):
-                yield
-
-
 @pytest.mark.usefixtures('xenial', 'returns_xenial')
 def test_list_versions(capsys):
     ret = rubyvenv.main(('--list',))
@@ -136,3 +218,47 @@ def test_list_versions(capsys):
         '    - 2.3.0\n'
         '    - 2.3.1\n'
     )
+
+
+@pytest.mark.usefixtures('xenial')
+def test_pick_version_version_specified():
+    assert rubyvenv.pick_version('2.3.0') == rubyvenv.Version(
+        '2.3.0',
+        'https://rvm.io/binaries/ubuntu/16.04/x86_64/ruby-2.3.0.tar.bz2',
+    )
+
+
+@pytest.mark.usefixtures('xenial', 'returns_xenial')
+def test_pick_version_latest():
+    assert rubyvenv.pick_version('latest') == rubyvenv.Version(
+        '2.3.1',
+        'https://rvm.io/binaries/ubuntu/16.04/x86_64/ruby-2.3.1.tar.bz2',
+    )
+
+
+def test_missing_dest_dir(capsys):
+    with pytest.raises(SystemExit):
+        rubyvenv.main(())
+    out, err = capsys.readouterr()
+    assert 'DEST_DIR is required' in out + err
+
+
+def _run(env, sh):
+    return subprocess.check_output((
+        'bash', '-euc',
+        'PS1="$ "; . {env}/bin/activate; {sh}'.format(env=env, sh=sh),
+    )).decode('UTF-8')
+
+
+@pytest.mark.usefixtures('mocked_cache')
+def test_integration(tmpdir):
+    env = tmpdir.join('rubyvenv')
+    envp = env.strpath
+    assert rubyvenv.main((envp,)) is None
+
+    assert _run(envp, 'echo $RUBYVENV') == envp + '\n'
+    assert _run(envp, 'which ruby') == env.join('bin/ruby').strpath + '\n'
+    assert _run(envp, 'which gem') == env.join('bin/gem').strpath + '\n'
+
+    _run(envp, 'gem install sass --no-ri --no-rdoc')
+    assert _run(envp, 'which sass') == env.join('bin/sass').strpath + '\n'
